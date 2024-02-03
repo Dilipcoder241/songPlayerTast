056d4536b4ff3e3691ea65cec03cfa1cc5d47ac6
dilip <mihirsingh241@gmail.com>
2024-02-03 19:06:01 +0530

    fix little bug for the video

--- src/components/Playing.jsx
+++ src/components/Playing.jsx
@@ -12,7 +12,7 @@ function Playing() {
       {currentSongName && <marquee className='border-2 border-[#31304D] w-fit py-3 px-5 text-center rounded-3xl hover:border-[#ffff]'>Now Playing {currentSongName.split('.')[0]}</marquee>}
       </div>
       <div className='absolute -bottom-[9.5vw] md:-bottom-[9.5vw] left-[50%] -translate-x-[50%]'>
-        <video ref={videoRef} muted loop src="/wave.webm" className='w-[80vw] mix-blend-lighten blur-sm rounded-3xl'></video>
+        <video ref={videoRef} muted loop src="/wave.webm" className='w-[80vw] mix-blend-lighten rounded-3xl'></video>
       </div>
     </div>
   )
